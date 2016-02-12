c304b0908aa39a788fe31bde0475b2fe78b936c8
Shaelyn <shaelynjoy@gmail.com>
2016-02-11 18:42:30 -0800

    Hello app

---
+++ AwesomeProject/app.js
@@ -0,0 +1,15 @@
+import React from "react-native";
+
+let { View, Text } = React;
+
+let App = React.createClass({
+	render: function () {
+		return (
+			<View>
+				<Text>Hello React!</Text>
+			</View>
+		);
+	}
+})
+
+export default App;
--- AwesomeProject/index.ios.js
+++ AwesomeProject/index.ios.js
@@ -11,21 +11,12 @@ import React, {
   View
 } from 'react-native';
 
+import App from "./app";
+
 class AwesomeProject extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <App />
     );
   }
 }
